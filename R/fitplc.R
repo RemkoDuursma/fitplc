1cd958169588f3a202b2060bd4c19f0e7be16168
Remko Duursma <remkoduursma@gmail.com>
2013-11-27 13:08:13 +1100

    added coef.manyplcfit. Updated test script.

--- R/fitplc.R
+++ R/fitplc.R
@@ -174,6 +174,18 @@ print.manyplcfit <- function(x,...){
 }
 
 
+#'@export
+coef.manyplcfit <- function(object, ...){
+  
+  x <- lapply(object,coef)
+  vf <- function(m)as.vector(t(m))
+  dfr <-  as.data.frame(do.call(rbind,lapply(x,vf)))
+  names(dfr) <- c("S","S_lower","S_upper","P50","P50_lower","P50_upper")
+  
+  return(dfr)
+}
+
+
 
 
 
--- test/testit.R
+++ test/testit.R
@@ -3,6 +3,7 @@
 # Stem at ROS
 stemros <- read.csv("test/stemvul-ros.csv")
 stemrosfits <- fitplcs(stemros, "Species", bootci=TRUE)
+stemrosfits
 
 windows(10,8)
 par(mfrow=c(3,1), mar=c(4,4,2,2))
@@ -11,6 +12,8 @@ dev.copy2pdf(file="output/stemrosfits.pdf")
 
 
 # only curves:
+windows()
+par(mfrow=c(1,1))
 plot(stemrosfits[[1]], plotdata=F, plotP50=F, linecol="blue")
 plot(stemrosfits[[2]], plotdata=F, plotP50=F, add=T, linecol="red")
 plot(stemrosfits[[3]], plotdata=F, plotP50=F, add=T, linecol="forestgreen")
@@ -25,7 +28,7 @@ f <- fitplcs(stemvul, "Species", bootci=TRUE)
 windows(10,8)
 par(mfrow=c(3,1), mar=c(4,4,2,2))
 for(i in 1:3)plot(f[[i]], xlim=c(0,7), main=names(f)[i])
-dev.copy2pdf(file="output/leafvulfits.pdf")
+dev.copy2pdf(file="output/stemvulfits.pdf")
 
 
 # leafvul
@@ -35,6 +38,46 @@ windows(10,8)
 par(mfrow=c(3,1), mar=c(4,4,2,2))
 for(i in 1:3)plot(f[[i]], xlim=c(0,7), main=names(f)[i])
 dev.copy2pdf(file="output/leafvulfits.pdf")
+
+# usleafvul
+usleafvul <- read.csv("test/usleafvul-points.csv", sep=";")
+f <- fitplcs(usleafvul, "Species", bootci=TRUE, 
+             varnames=c(PLC="CumulPercent", WP="MPa"))
+windows(10,8)
+par(mfrow=c(3,1), mar=c(4,4,2,2))
+for(i in 1:3)plot(f[[i]], xlim=c(0,7), main=names(f)[i])
+dev.copy2pdf(file="output/usleafvulfits.pdf")
+
+
+# usstemvul
+usstemvul <- read.csv("test/usstemvul-points.csv", sep=";")
+f <- fitplcs(usstemvul, "Species", bootci=TRUE, 
+             varnames=c(PLC="CumulPercent", WP="MPa"))
+windows(10,8)
+par(mfrow=c(3,1), mar=c(4,4,2,2))
+for(i in 1:3)plot(f[[i]], xlim=c(0,7), main=names(f)[i])
+dev.copy2pdf(file="output/usstemvulfits.pdf")
+
+
+
+
+
+
+# overlap <- function(vec1, vec2){
+#   
+#   min1 <- min(vec1)
+#   max1 <- max(vec1)
+#   vec2 <- c(min(vec2),max(vec2))
+#   
+#   res <- findInterval(min1, vec2) == 1|| findInterval(max1, vec2) == 1
+# return(res)
+# }
+# 
+# x <- c(1.5,3,4)
+# y <- c(1,2)
+# overlap(x,y)
+
+
 
 
 
