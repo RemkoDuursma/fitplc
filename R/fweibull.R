a3ec2002c440e0ef20b713fc905f1ed0e6051b78
Remko Duursma <remkoduursma@gmail.com>
2014-01-23 11:09:20 +1100

    x parameter now works; but sometimes does not converge.

--- R/fweibull.R
+++ R/fweibull.R
@@ -3,6 +3,7 @@
 #' @export
 fweibull <- function(P, SX, PX, X=50){
   
+  X <- X[1] # when fitting; vector may be passed but X cannot actually vary.
   V <- (X-100)*log(1-X/100)
   p <- (P/PX)^((PX*SX)/V)
   relk <- (1-X/100)^p
