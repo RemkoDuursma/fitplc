0ccb06a518d0562cab2edec6f739768c1eb08f2e
Remko Duursma <remkoduursma@gmail.com>
2016-11-23 11:47:40 +1100

    notes on shape parameters versus slope at inflection point in doc

--- R/weibull_sigmoidal.R
+++ R/weibull_sigmoidal.R
@@ -1,5 +1,5 @@
 #' Weibull vulnerability curve
-#' @description The Weibull function, as re-parameterized by Ogle et al. (2009), which describes the relative conductivity as a function of the plant water potential. The relative conductivity is scaled to be 1 when water potential is zero. 
+#' @description The Weibull function, as re-parameterized by Ogle et al. (2009), which describes the relative conductivity as a function of the plant water potential. The relative conductivity (relK) is scaled to be 1 when water potential is zero. The slope of relK versus P at the inflection point can be calculated from the shape parameter (SX) as slope = -SX/100.
 #' @param P Water potential (positive-valued MPa)
 #' @param SX Shape parameter
 #' @param PX Water potential at X loss of conductivity.
@@ -19,9 +19,9 @@ fweibull <- function(P, SX, PX, X=50){
 }
 
 #' Sigmoidal vulnerability curve
-#' @description A sigmoidal-exponential function, which describes the relative conductivity as a function of the plant water potential. The relative conductivity is scaled to be 1 when water potential is zero. This function was used by Pammenter and vander Willigen (1998), but note that this implementation gives the relative conductivity, not the PLC (but relK = 1 - PLC).
+#' @description A sigmoidal-exponential function, which describes the relative conductivity as a function of the plant water potential. The relative conductivity is scaled to be 1 when water potential is zero. This function was used by Pammenter and vander Willigen (1998), but note that this implementation gives the relative conductivity, not the PLC (but relK = 1 - PLC). The slope of relK versus P at the inflection point can be calculated from the shape parameter (a) as slope = -a/4.
 #' @param P Water potential (positive-valued MPa)
-#' @param a Shape parameter, related to the slope at the inflection point as slope = -a/4
+#' @param a Shape parameter, related to the slope at the inflection point (see Description).
 #' @param PX Water potential at X loss of conductivity (positive valued).
 #' @param X If 50, PX is the P50.
 #' @references Pammenter, N.W., Willigen, C.V. der, 1998. A mathematical and statistical analysis of the curves illustrating vulnerability of xylem to cavitation. Tree Physiol 18, 589-593. doi:10.1093/treephys/18.8-9.589
@@ -30,6 +30,7 @@ fweibull <- function(P, SX, PX, X=50){
 #' curve(fsigmoidal(x, PX=-2, a=2), add=T)
 fsigmoidal <- function(P, PX, a, X=50){
   
+  X <- X[1] # vector might have been passed but X cannot actually vary.
   P <- -P
   PX <- -PX
   b <- PX - (1/a)*(100/X - 1)
